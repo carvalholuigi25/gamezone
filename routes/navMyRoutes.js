453ee652ab7f1112c4b15fe1b46954cd780bc83b
Luigi Carvalho <carvalholuigi25@gmail.com>
2023-07-09 17:48:24 +0100

    custom header

--- routes/navMyRoutes.js
+++ routes/navMyRoutes.js
@@ -1,19 +1,30 @@
+import React from 'react';
+import Header from '../shared/header';
 import { Home, ReviewDetails, About } from '../screens';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerNavigationOptions } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
 
 export default function NavMyRoutes() {
-    const Drawer = createDrawerNavigator();
+    const myHeader = () => {
+        return {
+            headerTitle: (props) => {
+                return (
+                    <Header title={`GameZone`} />
+                )
+            }
+        }
+    };
+
+    const initParams = { key: '0' };
 
     return (
         <NavigationContainer>
-            <Drawer.Navigator 
-                initialRouteName="Home" 
-                backBehavior='firstRoute'
-            >
-                <Drawer.Screen name="Home" component={Home} options={{ title: 'Home' }} />
-                <Drawer.Screen name="ReviewDetails" initialParams={{ key: '0' }} component={ReviewDetails} options={{ title: 'Review Details' }} />
-                <Drawer.Screen name="About" component={About} options={{ title: 'About' }} />
+            <Drawer.Navigator>
+                <Drawer.Screen name="Home" component={Home} options={myHeader} />
+                <Drawer.Screen name="ReviewDetails" initialParams={initParams} component={ReviewDetails} options={myHeader} />
+                <Drawer.Screen name="About" component={About} options={myHeader} />
             </Drawer.Navigator>
         </NavigationContainer>
     );
